ab2bca0ec33dfd0e1f29938eb926ccd444094e35
Dylan Ang <dylanmouang@gmail.com>
2022-03-10 09:21:10 -0800

    Save all_rewards instead of losses twice

--- run_dqn_pong.py
+++ run_dqn_pong.py
@@ -92,6 +92,6 @@ for frame_idx in range(1, num_frames + 1):
 
 # Save losses and reward changes
 np.savetxt("losses.csv", losses, delimiter=",")
-np.savetxt("all_rewards.csv", losses, delimiter=",")
+np.savetxt("all_rewards.csv", all_rewards, delimiter=",")
 
 
